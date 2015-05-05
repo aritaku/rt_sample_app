7f9878fc3061aea2637dadd583473753e5c41cb0
aritaku19@gmail.com <aritaku19@gmail.com>
2015-05-06 01:50:15 +0900

    第3章_リスト3.1まで

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RtSampleApp::Application.config.secret_key_base = '21ffe9aca78438656a5c0be8b5b559aaacf8ddb27f41f6201b3f9c1efa7afba5d5c7c9ce61830b918ae51ae3f1f7b20e7c59d078993e1f3189b23b0327f65294'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RtSampleApp::Application.config.session_store :cookie_store, key: '_rt_sample_app_session'
